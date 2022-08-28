a2aa0e583a1acbdefd3d6e4d3137de8f78196752
Joel <Lianb1@nku.edu>
2022-08-28 17:05:02 -0400

    added .gitignore .gitattributes

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by Joel Lian on 8/27/22.
+//
+
+import UIKit
+
+class SettingsViewController: NSObject {
+
+}
---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Joel Lian on 8/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //amount of bill from txt field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        //calculate total tip from tip * tipPercentage
+        let tipPercentage = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        //update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+
+}
+
